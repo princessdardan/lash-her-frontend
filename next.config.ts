1735c9a19b62032bb976892b404df6b70da46263
Dardan Demiri <dardantheman@gmail.com>
2025-12-11 20:51:39 -0500

    added strapi cloud media url to next.config

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,16 @@ const nextConfig: NextConfig = {
     dangerouslyAllowLocalIP: true,
     remotePatterns: [
       {
-      protocol: "http",
-      hostname: "localhost",
-      port: "1337",
-      pathname: "/uploads/**/*",
-      }
+        protocol: "http",
+        hostname: "localhost",
+        port: "1337",
+        pathname: "/uploads/**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "cheerful-prosperity-b55f073006.media.strapiapp.com",
+        pathname: "/**",
+      },
     ],
   },
 };
